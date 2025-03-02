9da4d75630f53f840cbce51029b0c0181568621b
Abhinash Gupta <abhinashgupta1822@example.com>
2025-03-02 14:58:47 +0530

    Nested Structure

--- structure and union/structure/struct.c
+++ structure and union/structure/struct.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
 #include<string.h>
 struct person{
-    char name[20];
-    int age;
-    char address[100];
-    int salary;
+    char name[20]; //20 bytes
+    int age; //4 bytes
+    char address[100]; //100 bytes
+    int salary; //4 bytes
 };
+//Total memory occupies : 20+4+100+4 =128 bytes
+
+//nested Structures
+struct employee{
+    int emp_Id;
+    struct person p;
+};
+
 int main(){
     struct person p1;
     // p1.name = "Abhinash Gupta";  //In C, we cannot assign a string to a character array using = after declaring the structure.name and address are arrays, so direct assignment is not allowed.
@@ -20,5 +28,10 @@ int main(){
     printf("%s\n%d\n%s\n%d\n\n",p1.name, p1.age , p1.address, p1.salary);
 
     printf("Direct Initialization at Declaration : \n");
-    printf("%s\n%d\n%s\n%d\n",p2.name, p2.age , p2.address, p2.salary);
+    printf("%s\n%d\n%s\n%d\n\n",p2.name, p2.age , p2.address, p2.salary);
+
+    //Nested Structure initialization and declaration
+    struct employee e1={209981,"Kunal",21,"Bhopal",0};
+    printf("%d\n%s\n%d\n%s\n%d\n",e1.emp_Id,e1.p.name,e1.p.age , e1.p.address, e1.p.salary);
+    
 }
